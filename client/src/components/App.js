344f625692f35b4204bb82d56c0f9443fc73100d
jeff.smith <slightlyoffbass@gmail.com>
2019-09-25 13:48:39 -0500

    add empty components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const App = () =>{
-    return <div>App</div>;
+    return(
+        <BrowserRouter>
+            <div>App</div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
+
+// three types of routers in react-router-dom
+// - BrowserRouter - Uses everything after the TDL (.com,.net) or port as the 'path'
+// - HashRouter - users everything after a # as the 'path'
+// - - example: Use for github pages
+// - MemoryRouter - does not use URL to track navigation
