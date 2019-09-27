9545226a3ed1ea67d5c0f487624717608515137b
jeff.smith <slightlyoffbass@gmail.com>
2019-09-27 14:06:20 -0500

    add field set to create stream

--- client/README.md
+++
@@ -1,6 +0,0 @@
-## Client
-____
-
-### react-router-dom
-
-redux-devtools-extension - https://github.com/zalmoxisus/redux-devtools-extension
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = ( ) => {
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+
+    renderInput({input,label}){
+        return(
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+
+        ); 
+    }
+
+    render(){
+        return(
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Title "/>
+                <Field name="description" component={this.renderInput} label="Description "/>
+            </form>
+        );
+    }
 };
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
