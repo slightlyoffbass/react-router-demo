bec5256df6f956a13df16126e04a5adf9b49c053
jeff.smith <slightlyoffbass@gmail.com>
2019-10-02 08:27:12 -0500

    add action creators for crud functions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,14 @@
 import streams from '../apis/streams';
 
-import { SIGN_IN, SIGN_OUT,CREATE_STREAM } from './types';
+import { 
+    SIGN_IN, 
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAM, 
+    FETCH_STREAMS,
+    DELETE_STREAM,
+    EDIT_STREAM
+ } from './types';
 
 
 export const signIn = (userId) =>{
@@ -20,4 +28,25 @@ export const createStream = formValues => async dispatch => {
     const response = await streams.post('/streams', formValues);
 
     dispatch({ type: CREATE_STREAM, payload: response.data })
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({type: '/streams', payload: response.dat});
+};
+
+export const fetchStream = (id) => async dispatch =>{
+    const respone = await streams.get(`/streams/${id}`);
+    dispatch({type: FETCH_STREAM, payload: respone.data})''
+};
+
+export const ecditStream = (id, formValues) => dispatch =>{
+    const response = await streams.put(`/streams/${id}`);   
+    dispatch ({type: EDIT_STREAM, payload: response.data});
+};
+
+export const deleteStream = (id) => async dispatch =>{
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id});
 };
