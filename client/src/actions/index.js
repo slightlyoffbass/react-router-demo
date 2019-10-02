a610c7fec751a5df95407856cb949dae8acf4bd4
jeff.smith <slightlyoffbass@gmail.com>
2019-10-02 08:30:20 -0500

    fix second typo

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -32,7 +32,7 @@ export const createStream = formValues => async dispatch => {
 
 export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
-    dispatch({type: '/streams', payload: response.dat});
+    dispatch({type: FETCH_STREAMS, payload: response.dat});
 };
 
 export const fetchStream = (id) => async dispatch =>{
