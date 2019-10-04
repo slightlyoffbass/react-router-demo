7c62d17368ee7720ff66504d436694c418cf1f0c
slightlyoffbass <slightlyoffbass@gmail.com>
2019-10-04 15:54:32 -0500

    set up edit streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -41,9 +41,10 @@ export const fetchStream = (id) => async dispatch =>{
     dispatch({type: FETCH_STREAM, payload: respone.data});
 };
 
-export const ecditStream = (id, formValues) => async dispatch =>{
-    const response = await streams.put(`/streams/${id}`);   
+export const editStream = (id, formValues) => async dispatch =>{
+    const response = await streams.patch(`/streams/${id}`,formValues);   
     dispatch ({type: EDIT_STREAM, payload: response.data});
+    history.push('/');
 };
 
 export const deleteStream = (id) => async dispatch =>{
