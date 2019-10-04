cf94c766fc7de9f787a4a8fe4520f68cc228d14b
jeff.smith <slightlyoffbass@gmail.com>
2019-10-04 11:09:07 -0500

    add create stream button

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,9 +24,9 @@ export const signOut = () =>{
     };
 };
 
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
-
+export const createStream = formValues => async (dispatch, getState) => {
+    const {userId} = getState().auth;
+    const response = await streams.post('/streams', {...formValues, userId});
     dispatch({ type: CREATE_STREAM, payload: response.data })
 };
 
@@ -47,6 +47,5 @@ export const ecditStream = (id, formValues) => async dispatch =>{
 
 export const deleteStream = (id) => async dispatch =>{
     await streams.delete(`/streams/${id}`);
-
     dispatch({ type: DELETE_STREAM, payload: id});
 };
