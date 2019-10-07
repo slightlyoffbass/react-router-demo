16794ddf7056bc45e15b0ff8d4fc727a2c4cdeac
jeff.smith <slightlyoffbass@gmail.com>
2019-10-07 09:52:18 -0500

    add variables to props of modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -5,18 +5,17 @@ import history from '../history';
 const Modal = (props) =>{
     return ReactDOM.createPortal(
         <div 
-            onClick={()=> history.push('/')} 
+            onClick={props.onDismiss} 
             className="ui dimmer modals visible active">
 
             <div  onClick={(e)=> e.stopPropagation()}  className="ui standard modal visible active">
                  <div className="header"> {props.title} </div>
-                    <div className="content">
-                        {props.content}
-                    </div>
-                    <div className="actions">
-                        {props.actions}
-                    </div>
-                
+                <div className="content">
+                    {props.content}
+                </div>
+                <div className="actions">
+                    {props.actions}
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = ( ) => {
     const actions = (
-        <div>
+        <React.Fragment>
             <button className="ui button negative">Delete</button>
             <button className="ui button">Cancel</button>
-        </div>
+        </React.Fragment>
     )
 
     return (
     <Modal
+        onDismiss={()=> history.push('/')}
         title="Delete Stream"
         content="Are you sure you want to delete this stream?"
         actions={actions}
